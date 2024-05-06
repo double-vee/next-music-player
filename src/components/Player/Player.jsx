0cbde05a76b40d46a280dfe5d7c8515b8aa8fb31
Weronika Kansy <weronika.kansy@gmail.com>
2024-05-06 02:20:00 +0200

    Added states for audio data and current track data

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,10 +1,18 @@
+'use client';
+
+import { useState } from 'react';
+
 import Controls from '@/components/Controls/Controls';
 import Track from '@/components/Track/Track';
+import { data } from '@/data';
 
 export default function Player() {
+  const [tracks, setTracks] = useState(data());
+  const [currentTrack, setCurrentTrack] = useState(tracks[0]);
+
   return (
     <section>
-      <Track />
+      <Track currentTrack={currentTrack} />
       <Controls />
     </section>
   );
